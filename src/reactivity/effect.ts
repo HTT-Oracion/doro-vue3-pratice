96029c3e57a09b67981d43666636d532699d769d
lvjinbin <1367100043@qq.com>
2023-04-05 15:40:40 +0800

    feat: init project & add reactive、readonly、effect tests

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,85 @@
+import { extend } from "../shared";
+
+class ReactiveEffect {
+  private _fn: any;
+  deps = [];
+  active = true;
+  onStop?: () => void;
+  constructor(fn, public scheduler?) {
+    this._fn = fn;
+  }
+
+  run() {
+    activeEffect = this;
+    return this._fn();
+  }
+
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
+}
+
+const targetMap = new Map();
+
+// 收集依赖 （get）
+export function track(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()));
+  }
+  let dep = depsMap.get(key);
+
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  if (!activeEffect) return;
+
+  dep.add(activeEffect);
+  activeEffect.deps.push(dep);
+}
+
+// 触发依赖
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
+
+  for (const effect of dep) {
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
+  }
+}
+
+// 当前触发effect的实例
+let activeEffect;
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler);
+  // _effect.onStop = options.onStop;
+
+  extend(_effect, options);
+
+  _effect.run();
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
+}
